a7ff30cd6623a58b369ce7117ef58718664bead3
Chris Jacobi <106036496+ChrisJacobi@users.noreply.github.com>
2022-07-13 21:34:58 -0400

    added filler photo and text

--- script.js
+++ script.js
@@ -1,2 +1,18 @@
 
 
+const hamBtn = document.querySelectorAll('hamburgerBtn');
+
+
+hamBtn.addEventListener('click', function() {
+
+	const navTab = document.getElementsByClassName('navTab');
+
+	if (navTab.classList.contains("dNone")) {
+		navTab.classList.remove("dNone");
+	}
+	else {
+		navTab.classList.add("dNone");
+	}
+
+});
+
